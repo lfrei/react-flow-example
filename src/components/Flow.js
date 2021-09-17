ec98ecf0be60e20c8aece5fbf3b739b384788d77
Lukas Frei <lukas.frei@zuehlke.com>
2021-09-17 12:05:46 +0200

    Update flow example

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactFlow from 'react-flow-renderer';
 
 const elements = [
-  { id: '1', type: 'input', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
-  { id: '2', data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-];
-
+    {
+      id: '1',
+      type: 'input',
+      data: { label: 'Start' },  
+      position: { x: 250, y: 25 },
+    },
+    {
+      id: '2',
+      data: { label: 'Milestone' },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'output',
+      data: { label: 'End' },
+      position: { x: 250, y: 250 },
+    },
+    { id: 'e1-2', source: '1', target: '2', animated: true },
+    { id: 'e2-3', source: '2', target: '3', animated: true },
+  ];
 
 function Flow() {
     return (
